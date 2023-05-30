52d63022124479aa81278ba2d38e8d5b41e24b16
Yamin <myamin335@gmail.com>
2023-05-30 23:48:06 +0200

    Photo picker availability checker removed

--- app/src/main/java/mollah/yamin/androidnativemediapicker/ui/MainActivity.kt
+++ app/src/main/java/mollah/yamin/androidnativemediapicker/ui/MainActivity.kt
@@ -2,8 +2,6 @@ package mollah.yamin.androidnativemediapicker.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import mollah.yamin.androidnativemediapicker.R
 import mollah.yamin.androidnativemediapicker.databinding.MainActivityBinding
@@ -18,15 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (!ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(applicationContext)) {
-            Toast.makeText(this, "Photo picker is not available on this device!", Toast.LENGTH_LONG).show()
-        }
-
         binding.btnSingleImage.setOnClickListener {
-            if (!ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(applicationContext)) {
-                Toast.makeText(this, "Photo picker is not available on this device!", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
             val nIntent = Intent(this@MainActivity, MediaViewActivity::class.java)
             nIntent.putExtra(MEDIA_SELECTION_TYPE, 1)
             startActivity(nIntent)
@@ -34,10 +24,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnMultipleImage.setOnClickListener {
-            if (!ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(applicationContext)) {
-                Toast.makeText(this, "Photo picker is not available on this device!", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
             val nIntent = Intent(this@MainActivity, MediaViewActivity::class.java)
             nIntent.putExtra(MEDIA_SELECTION_TYPE, 2)
             startActivity(nIntent)
@@ -45,10 +31,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnSingleVideo.setOnClickListener {
-            if (!ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(applicationContext)) {
-                Toast.makeText(this, "Photo picker is not available on this device!", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
             val nIntent = Intent(this@MainActivity, MediaViewActivity::class.java)
             nIntent.putExtra(MEDIA_SELECTION_TYPE, 3)
             startActivity(nIntent)
@@ -56,10 +38,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnMultipleVideo.setOnClickListener {
-            if (!ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(applicationContext)) {
-                Toast.makeText(this, "Photo picker is not available on this device!", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
             val nIntent = Intent(this@MainActivity, MediaViewActivity::class.java)
             nIntent.putExtra(MEDIA_SELECTION_TYPE, 4)
             startActivity(nIntent)
@@ -67,10 +45,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnSingleMedia.setOnClickListener {
-            if (!ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(applicationContext)) {
-                Toast.makeText(this, "Photo picker is not available on this device!", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
             val nIntent = Intent(this@MainActivity, MediaViewActivity::class.java)
             nIntent.putExtra(MEDIA_SELECTION_TYPE, 5)
             startActivity(nIntent)
@@ -78,10 +52,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnMultipleMedia.setOnClickListener {
-            if (!ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(applicationContext)) {
-                Toast.makeText(this, "Photo picker is not available on this device!", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
             val nIntent = Intent(this@MainActivity, MediaViewActivity::class.java)
             nIntent.putExtra(MEDIA_SELECTION_TYPE, 6)
             startActivity(nIntent)
